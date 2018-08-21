995472d1c45f6e3b32dae8b7f6b76713cedcb7a7
Dan Noguerol <dnoguerol@pivotal.io>
2018-08-21 16:07:53 +0200

    Updated blue and green to pre-prod and prod.

--- ci/prod-www.sh
+++ ci/prod-www.sh
@@ -4,20 +4,23 @@ export TERM=${TERM:-dumb}
 echo "Logging into CF"
 cf login -a $CF_API -o $CF_ORG -s $CF_SPACE -u $CF_USER -p $CF_PASSWD
 
-echo "Checking for blue www deployment"
+echo "Checking for pre-prod www deployment"
 cf check-route $CF_WWW_NAME_BLUE $CF_DOMAIN | grep 'does exist' &> /dev/null
 if [ $? == 0 ]; then
 	set -e
-	echo "Adding blue route to www"
+	echo "Adding pre-prod route to prod"
 	cf map-route $CF_WWW_NAME_BLUE $CF_DOMAIN -n $CF_WWW_NAME_GREEN
-	echo "Removing green www route"
-	cf unmap-route $CF_WWW_NAME_GREEN $CF_DOMAIN -n $CF_WWW_NAME_GREEN
-	echo "Deleting green www"
-	cf delete $CF_WWW_NAME_GREEN -f
-	echo "Renaming blue www to green"
+	cf check-route $CF_WWW_NAME_GREEN $CF_DOMAIN | grep 'does exist' &> /dev/null
+	if [ $? == 0 ]; then
+		echo "Removing prod route"
+		cf unmap-route $CF_WWW_NAME_GREEN $CF_DOMAIN -n $CF_WWW_NAME_GREEN
+		echo "Deleting prod app"
+		cf delete $CF_WWW_NAME_GREEN -f
+	fi
+	echo "Renaming pre-prod app to prod"
 	cf rename $CF_WWW_NAME_BLUE $CF_WWW_NAME_GREEN
-	echo "Removing blue www route"
+	echo "Removing pre-prod route"
 	cf delete-route $CF_DOMAIN -n $CF_WWW_NAME_BLUE -f
 else
-	echo "No blue www route found; skipping"
+	echo "No pre-prod www route found; skipping"
 fi
