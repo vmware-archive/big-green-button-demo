673731941bd1beb7983a43d6c0064b3fb511416e
Dan Noguerol <dnoguerol@pivotal.io>
2018-08-21 15:48:25 +0200

    Made more resilient for first-time deploys.

--- ci/prod-service.sh
+++ ci/prod-service.sh
@@ -4,20 +4,24 @@ export TERM=${TERM:-dumb}
 echo "Logging into CF"
 cf login -a $CF_API -o $CF_ORG -s $CF_SPACE -u $CF_USER -p $CF_PASSWD
 
-echo "Checking for blue service deployment"
+echo "Checking for pre-prod service deployment"
 cf check-route $CF_SERVICE_NAME-blue $CF_DOMAIN | grep 'does exist' &> /dev/null
 if [ $? == 0 ]; then
 	set -e
-	echo "Adding blue route to service"
+	echo "Adding pre-prod route to service"
 	cf map-route $CF_SERVICE_NAME-blue $CF_DOMAIN -n $CF_SERVICE_NAME
-	echo "Removing green service route"
-	cf unmap-route $CF_SERVICE_NAME $CF_DOMAIN -n $CF_SERVICE_NAME
-	echo "Deleting green service"
-	cf delete $CF_SERVICE_NAME -f
-	echo "Renaming blue service to green"
+	echo "Checking for prod service deployment"
+	cf check-route $CF_SERVICE_NAME $CF_DOMAIN | grep 'does exist' &> /dev/null
+	if [ $? == 0 ]; then
+		echo "Removing prod service route"
+		cf unmap-route $CF_SERVICE_NAME $CF_DOMAIN -n $CF_SERVICE_NAME
+		echo "Deleting prod service"
+		cf delete $CF_SERVICE_NAME -f
+	fi
+	echo "Renaming pre-prod service to prod"
 	cf rename $CF_SERVICE_NAME-blue fussball-service
-	echo "Removing blue service route"
+	echo "Removing pre-prod service route"
 	cf delete-route $CF_DOMAIN -n $CF_SERVICE_NAME-blue -f
 else
-	echo "No blue service route found; skipping"
+	echo "No pre-prod service route found; skipping"
 fi
