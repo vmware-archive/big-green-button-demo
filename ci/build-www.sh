1869cbc91f4c81a32cd7425cc8fcc5c7cc20a42d
Dan Noguerol <dnoguerol@pivotal.io>
2018-09-14 11:56:15 +0200

    Removed npm install from www build.

--- ci/build-www.sh
+++ ci/build-www.sh
@@ -2,7 +2,6 @@
 cd big-green-www/www
 ln -s /opt/node_modules node_modules
 echo "export const environment = { production: true, apiPrefix: '$API_PREFIX', mapsApiKey: '$GOOGLE_MAPS_API_KEY' };" > src/environments/environment.prod.ts
-npm install
 ng build --source-map=false --configuration=production --preserve-symlinks
 ng build --configuration=production-en --preserve-symlinks
 ng build --configuration=production-nl --preserve-symlinks
