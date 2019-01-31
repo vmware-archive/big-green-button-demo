b0e3c136c80fb56c2d2a55f5e4ffaf260c529e62
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-01-31 15:29:55 +0100

    Added metrics and logging for testing purposes.

--- fussball-service/src/main/java/com/pivotal/demo/FussballService/FussballServiceApplication.java
+++ fussball-service/src/main/java/com/pivotal/demo/FussballService/FussballServiceApplication.java
@@ -1,11 +1,34 @@
 package com.pivotal.demo.FussballService;
 
+import io.micrometer.core.instrument.Metrics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class FussballServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(FussballServiceApplication.class, args);
-	}
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    private final Logger logger = LoggerFactory.getLogger(FussballServiceApplication.class);
+
+    @Bean
+    ApplicationRunner runner() {
+        // periodically update a metric and emit a log message for testing
+        return args -> {
+            this.executorService.scheduleAtFixedRate(() -> {
+                Metrics.counter("app.fussball.count").increment();
+                logger.info("Fussball service PING");
+            }, 1, 10, TimeUnit.SECONDS);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FussballServiceApplication.class, args);
+    }
 }
